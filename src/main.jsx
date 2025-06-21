3f1eab13d69dc77eb054d3371bea3984caa12a62
Drexxxl <wersolle@gmail.com>
2025-06-21 09:10:55 +0300

    Add basic React setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SpaceJumpMainMenu from './components/SpaceJumpMainMenu';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SpaceJumpMainMenu />
+  </React.StrictMode>
+);
