26548135d06b40cb40716069f6698ab0e7843fe0
Drexxxl <wersolle@gmail.com>
2025-06-21 10:33:41 +0300

    Revert "Fix docs and style tag"

--- src/components/SpaceJumpMainMenu.jsx
+++ src/components/SpaceJumpMainMenu.jsx
@@ -158,7 +158,7 @@ export default function SpaceJumpMainMenu() {
         )}
       </AnimatePresence>
 
-      <style>{`
+      <style jsx>{`
         .stars-layer {
           position: absolute;
           width: 100%;
