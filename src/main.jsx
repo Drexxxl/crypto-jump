a707d16a9c9d06ff52efb3f1e19bf0187e25bbac
Drexxxl <wersolle@gmail.com>
2025-06-21 10:19:02 +0300

    Enable Telegram fullscreen

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SpaceJumpMainMenu from './components/SpaceJumpMainMenu';
+import { viewport } from '@telegram-apps/sdk';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SpaceJumpMainMenu />
   </React.StrictMode>
 );
+
+// Request fullscreen in supported Telegram clients
+try {
+  viewport?.requestFullscreen?.();
+} catch (e) {
+  // ignore errors if the method is unavailable
+}
