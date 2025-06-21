0779cfba42176045f31308e91c0a7dda60b6d3ab
Drexxxl <wersolle@gmail.com>
2025-06-21 09:15:54 +0300

    Improve main menu styling

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SpaceJumpMainMenu from './components/SpaceJumpMainMenu';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SpaceJumpMainMenu />
+  </React.StrictMode>
+);
